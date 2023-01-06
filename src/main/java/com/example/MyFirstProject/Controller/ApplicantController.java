d02c1f5998b70d44655e708ccd5863a433b77deb
thara-1234 <tharakrishna1989@gmail.com>
2023-01-06 11:37:29 +0530

    Controller & Service completed

--- src/main/java/com/example/MyFirstProject/Controller/ApplicantController.java
+++ src/main/java/com/example/MyFirstProject/Controller/ApplicantController.java
@@ -1,4 +1,21 @@
 package com.example.MyFirstProject.Controller;
 
+import com.example.MyFirstProject.Entity.Applicant;
+import com.example.MyFirstProject.Service.ApplicantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
 public class ApplicantController {
+    @Autowired
+    ApplicantService applicantService;
+    @RequestMapping(value = "/applicants",method = RequestMethod.POST)
+    public void createApplicant(@RequestBody Applicant applicant){
+        applicantService.createAnApplicant(applicant);
+    }
 }
--- src/main/java/com/example/MyFirstProject/Service/ApplicantService.java
+++ src/main/java/com/example/MyFirstProject/Service/ApplicantService.java
@@ -1,4 +1,16 @@
 package com.example.MyFirstProject.Service;
 
+import com.example.MyFirstProject.Entity.Applicant;
+import com.example.MyFirstProject.Repository.ApplicantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ApplicantService {
+    @Autowired
+    ApplicantRepository applicantRepository;
+    public void createAnApplicant(Applicant applicant){
+        applicantRepository.save(applicant);
+    }
+
 }
