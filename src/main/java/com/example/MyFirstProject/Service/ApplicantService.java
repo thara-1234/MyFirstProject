ffdd07858ab324f02dc3936246d9c288623be464
ramshadvp <ramshad.vp@innovaturelabs.com>
2023-01-09 13:12:00 +0530

    Check the diff

--- src/main/java/com/example/MyFirstProject/Service/ApplicantService.java
+++ src/main/java/com/example/MyFirstProject/Service/ApplicantService.java
@@ -1,74 +1,38 @@
 package com.example.MyFirstProject.Service;
 
 import com.example.MyFirstProject.Entity.Applicant;
-import com.example.MyFirstProject.Entity.LoginApplicant;
 import com.example.MyFirstProject.Repository.ApplicantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Period;
 
-import static java.time.LocalDate.parse;
-
 @Service
 public class ApplicantService {
     @Autowired
     ApplicantRepository applicantRepository;
-     Applicant applicant;
-
-     public void createAnApplicant(Applicant applicant) {
-
-            if (applicant.getPhoneNo().length() != 10) {
-                throw new IllegalArgumentException("Invalid Phone Number");
-            }
-
-            if (!applicant.getEmailId().endsWith(".com")) {
-                throw new IllegalArgumentException("Invalid Mail ID");
+    public static boolean checkPhNo(Applicant applicant){
+        if(applicant.getPhone_no().length() == 10){
+            return true;
         }
-
-            LocalDate CurrDate = LocalDate.now();
-            LocalDate dateOfBirth= applicant.getDob();
-           Period p=Period.between(dateOfBirth, CurrDate);
-           applicant.setAge(p.getYears());
-
-           if(p.getYears()<18){
-               throw new IllegalArgumentException("Not Eligible");
-           }
-           if(applicant.getZipcode().length()!=6){
-               throw new IllegalArgumentException("Invalid Zipcode");
-           }
-
-        if(applicant.getId()==0 || applicant.getPhoneNo()==null||applicant.getZipcode()==null||applicant.getAge()==0
-                ||applicant.getEmailId()==null||applicant.getAddress()==null||applicant.getGender()==null||
-        applicant.getName()==null||applicant.getDob()==null){
-            throw new IllegalArgumentException("Values cannot be null");
-           }
-        if(applicant.getPassword().length()!=6 &&  applicant.getPassword().matches("[a-zA-Z0-9]+")){
-            throw new IllegalArgumentException("Invalid Password");
-
-        }
-        applicantRepository.save(applicant);
+        return false;
     }
-//    public String loginApplicant(LoginApplicant loginApplicant){
-//
-//        if(applicantRepository.login(loginApplicant.getEmail(),loginApplicant.getPassword())==1) {
-//            return loginApplicant.getEmail();
-//        }
-//            else {
-//               throw new IllegalArgumentException("Invalid data");
-//        }}}
-
-
-    public Applicant loginApplicant(LoginApplicant loginApplicant){
-
-        return applicantRepository.login(loginApplicant.getEmail(),loginApplicant.getPassword());
-      }
-
-}
-
-
+        public static boolean checkEmail(Applicant applicant){
+        if(applicant.getEmail_id().endsWith(".com")) {
+            return true;
+        }
+        return false;
+    }
+    public void createAnApplicant(Applicant applicant) {
+        try{
+        if(ApplicantService.checkPhNo(applicant)&& ApplicantService.checkEmail(applicant)){
+            applicantRepository.save(applicant);
+        }}
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
 
 
@@ -80,6 +44,38 @@ public class ApplicantService {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*public static int ageCal(Applicant applicant){
+        LocalDate CurrDate = LocalDate.now();
+        Long Year1= Long.valueOf(CurrDate.getYear());
+        LocalDate dob=LocalDate.parse()
+        Long Year2= Long.valueOf(dob.getYear());
+        int age= (int) (Year1-Year2);
+        return age;
+    }*/
+
+
+
+
+
+
+    }
 
 
 
